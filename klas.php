e335b2fc08847bc24e5142d6c8f938a510d05bf1
Candace <2100978@talnet.nl>
2020-02-05 12:18:55 +0100

    Uitwerking van de exercise

--- klas.php
+++ klas.php
@@ -6,12 +6,10 @@ $klas = readline("Wie zit er in de klas?" . PHP_EOL);
 $klasgenoten = array();
 
 
-// echo "de studenten in de klas zijn:" . PHP_EOL . $klasgenoten;
 
 for ($i = 0; $i <= $klas; $i++) {
 
     array_push($klasgenoten, $klas);
-    // echo ($klasgenoten);
 }
 
 foreach ($klasgenoten as $v) {
